309fb255a0466a11883e5b2e7a776f8b3240d475
Shakir Hanif <shakirhanif@hotmail.com>
2023-01-23 00:35:33 +0500

    setup adduser connection etc

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("hello world")
+const { Server } = require("socket.io");
+
+const io = new Server(4000, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+let users = [];
+
+const addUser = (account, socketId) => {
+  !users.some((user) => user.sub === account.sub) &&
+    users.push({ ...account, socketId });
+  console.log(users);
+};
+
+io.on("connection", (socket) => {
+  console.log(`user connected:${socket.id}`);
+  socket.on("addUser", (account) => {
+    addUser(account, socket.id);
+    io.emit("getUsers", users);
+  });
+});
