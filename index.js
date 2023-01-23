05fcaaad081a23133da16114d5fe8d906deac8d0
Shakir Hanif <shakirhanif@hotmail.com>
2023-01-23 23:26:39 +0500

    send message functionality

--- index.js
+++ index.js
@@ -14,10 +14,19 @@ const addUser = (account, socketId) => {
   console.log(users);
 };
 
+const getUser = (userId) => {
+  const myUser = users.find((user) => user.sub === userId);
+  return myUser;
+};
+
 io.on("connection", (socket) => {
   console.log(`user connected:${socket.id}`);
   socket.on("addUser", (account) => {
     addUser(account, socket.id);
     io.emit("getUsers", users);
+  });
+  socket.on("sendMessage", (data) => {
+    const user = getUser(data.receiverId);
+    io.to(user.socketId).emit("getMessage", data);
   });
 });
